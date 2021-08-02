207537db331987efc6277e326ae27778c74eee4e
guiyong <1432470220@qq.com>
2021-08-02 18:35:10 +0800

    dev3

--- app/src/main/java/com/ex/myapplication/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/ex/myapplication/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment Demo"
+        value = "THIS"
     }
     val text: LiveData<String> = _text
 }
